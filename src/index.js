b74bfdddb1a51439964bb8404793877b2b536631
kyuf <jeff.jl.wen@gmail.com>
2017-01-10 22:26:37 -0800

    added import for reducers and created store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import tictacfoeApp from './reducers/index';
 import App from './App';
+
+let store = createStore(tictacfoeApp);
 
 ReactDOM.render(
   <App />,
