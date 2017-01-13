3cef5d4e9b7fbdc2ed6511d296c9a1b752faf57c
kyuf <jeff.jl.wen@gmail.com>
2017-01-13 12:49:33 -0800

    changed and to & in title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="main-container">
-        <h1>X's and O's</h1>
+        <h1>X's & O's</h1>
         <BoardContainer />
         <ScoreboardContainer />
       </div>
