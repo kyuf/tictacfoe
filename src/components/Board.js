b86fd088b6351c6b9ed9279fb6478e31602d600a
kyuf <jeff.jl.wen@gmail.com>
2017-01-14 15:06:05 -0800

    refactored board space rendering to be more concise

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,41 +2,19 @@ import React, { PropTypes } from 'react';
 import Space from './Space'
 
 const Board = ({ spaces, onSpaceClick, roundOver }) => {
+  const nums = [0, 1, 2];
   return (
     <div className="board-container">
-      <div className="board-row">
-      	<Space
-      		value={spaces[0]}
-      		onClick={() => onSpaceClick(0)} />
-      	<Space
-      		value={spaces[1]}
-      		onClick={() => onSpaceClick(1)} />
-      	<Space 
-      		value={spaces[2]}
-      		onClick={() => onSpaceClick(2)} />
-      </div>
-      <div className="board-row">
-      	<Space
-      		value={spaces[3]}
-      		onClick={() => onSpaceClick(3)} />
-      	<Space
-      		value={spaces[4]}
-      		onClick={() => onSpaceClick(4)} />
-      	<Space
-      		value={spaces[5]}
-      		onClick={() => onSpaceClick(5)} />
-      </div>
-      <div className="board-row">
-      	<Space
-      		value={spaces[6]}
-      		onClick={() => onSpaceClick(6)} />
-      	<Space
-      		value={spaces[7]}
-      		onClick={() => onSpaceClick(7)} />
-      	<Space
-      		value={spaces[8]}
-      		onClick={() => onSpaceClick(8)} />
-      </div>
+      {nums.map((i) => (
+        <div className="board-row" key={i}>
+          {nums.map((j) => (
+            <Space
+              value={spaces[i*3+j]}
+              key={i*3+j}
+              onClick={() => onSpaceClick(i*3+j)} />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
